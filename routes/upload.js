54c6f39535bd3175e78085a1f4d909800c9f5cc1
btarg <icrazyblaze1@gmail.com>
2024-05-19 18:06:13 +0100

    fix: moved existing file checks into the upload endpoint rather than checking on file parse: existing files no longer double-upload

--- routes/upload.js
+++ routes/upload.js
@@ -1,24 +1,89 @@
 const express = require('express');
 const parseAndUpload = require('../services/fileParser');
 const { checkAuthenticated } = require('../routes/auth')
+const fileService = require('../services/fileService');
+const numberFromPSQL = require('../utils/conversions');
+const { getFullHostname } = require('../utils/hostname');
+
+const userModel = require('../models/userModel');
 
 require('dotenv').config();
+
 
 const router = express.Router();
 
 router.post('/', async (req, res) => {
     console.log("Hit upload endpoint");
+
+    // these are the same headers that will be passed to the parser
+    const fileHash = req.headers['filehash'];
+    const fileSize = Number(req.headers['filesize']);
+    const guildId = req.headers['guildid'];
+    const channelId = req.headers['channelid'];
+    const userId = req.headers['userid'];
+    const isDM = req.headers['isdm'];
+
+    const hostname = getFullHostname(req.hostname);
+
+    let user;
     try {
-        const data = await parseAndUpload(req);
-        // pass the data directly on success. the json should include downloadLink
-        res.status(200).json(data);
+        user = await userModel.getUserById(userId);
+        if (!user) {
+            console.log("User not found");
+            return res.status(404).send("User not found");
+        }
     } catch (error) {
-        console.log(error);
-        res.status(error.statusCode || 500).json({
-            message: "An error occurred.",
-            error
-        });
+        console.log("Error getting user: " + error);
+        return res.status(500).send("Error getting user");
     }
+
+    // Check if a file with the same hash already exists in this guild
+    fileService.getFileByHash(fileHash, guildId)
+        .then(async existingFile => {
+            if (existingFile) {
+                // If the file already exists, return the original copy
+                const downloadLink = `${hostname}/download/${existingFile.fileid}`; // remember, no capitals here!
+                console.log("Existing download link: " + downloadLink);
+                fileService.emitFileUploaded(channelId, userId, isDM, existingFile.filename, downloadLink);
+                return res.status(200).json({ message: 'File already exists', downloadLink: downloadLink });
+            }
+            else {
+                console.log("File does not already exist");
+                var bytesUsed = numberFromPSQL(user.bytesUsed);
+                var bytesAllowed = numberFromPSQL(user.bytesAllowed);
+                const bytesAboutToBeUsed = bytesUsed + fileSize;
+
+                console.log("FILE SIZE IS " + fileSize);
+                console.log("BYTES USED IS " + bytesUsed);
+                console.log("BYTES ALLOWED IS " + bytesAllowed);
+
+                console.log("Bytes used plus file size: " + bytesAboutToBeUsed);
+                if (bytesAboutToBeUsed >= bytesAllowed) {
+                    console.log("User exceeded storage limit");
+                    return res.status(403).send("User has exceeded their storage limit");
+                }
+
+                try {
+                    const data = await parseAndUpload(req, user);
+                    // pass the data directly on success. the json should include downloadLink
+                    return res.status(200).json(data);
+                } catch (error) {
+                    console.log(error);
+                    return res.status(error.statusCode || 500).json({
+                        message: "An error occurred.",
+                        error
+                    });
+                }
+
+            }
+
+        })
+        .catch(error => {
+            console.error('Error getting file by hash:', error);
+            return res.status(500).send(error);
+        });
+
+
 });
 
 module.exports = router;
--- services/fileParser.js
+++ services/fileParser.js
@@ -1,24 +1,18 @@
+require("dotenv").config();
+const { accessKeyId, secretAccessKey, region, Bucket } = require('../config/aws');
 const formidable = require('formidable');
 const { Upload } = require("@aws-sdk/lib-storage");
 const { S3Client, S3 } = require("@aws-sdk/client-s3");
 const Transform = require('stream').Transform;
-
 const fileService = require('../services/fileService');
 const userModel = require('../models/userModel');
 const fileModel = require('../models/fileModel');
-
 const { isLinkValid, generateId } = require('../routes/linkgenerator');
 const { getFullHostname } = require('../utils/hostname');
-
-require("dotenv").config();
-
 const { getIo } = require('../config/socket');
 const io = getIo();
 
-const { accessKeyId, secretAccessKey, region, Bucket } = require('../config/aws');
-const numberFromPSQL = require('../utils/conversions');
-
-const parseAndUpload = async (req) => {
+const parseAndUpload = async (req, user) => {
     return new Promise(async (resolve, reject) => {
         let options = {
             maxFileSize: (process.env.MB_MAX || 100) * 1024 * 1024, //100 megabytes converted to bytes,
@@ -30,8 +24,9 @@ const parseAndUpload = async (req) => {
         const fileSize = Number(req.headers['filesize']);
         const guildId = req.headers['guildid'];
         const channelId = req.headers['channelid'];
-        const userId = req.headers['userid'];
         const isDM = req.headers['isdm'];
+
+        const userId = user.id;
 
         const form = formidable(options);
 
@@ -43,20 +38,6 @@ const parseAndUpload = async (req) => {
             req.session.linkId = fileId;
         } else {
             fileService.invalidateLink(fileId);
-        }
-
-        let user;
-        try {
-            user = await userModel.getUserById(userId);
-            if (!user) {
-                console.log("User not found");
-                reject({ error: 'User not found', statusCode: 404 });
-                return;
-            }
-        } catch (error) {
-            console.log("Error getting user: " + error);
-            reject({ error: 'Error getting user', statusCode: 500 });
-            return;
         }
 
         // this will be set later
@@ -93,110 +74,71 @@ const parseAndUpload = async (req) => {
             originalFilename = file.originalFilename;
             console.log(`S3 KEY IS ${guildId}/${userId}/${originalFilename}`);
 
-            let fileExists = false;
-
-            // Check if a file with the same hash already exists in this guild
-            fileService.getFileByHash(fileHash, guildId)
-                .then(existingFile => {
-                    console.log("Existing file: " + existingFile);
-                    if (existingFile) {
-                        // If the file already exists, return the original copy
-                        const downloadLink = `${hostname}/download/${existingFile.fileid}`; // remember, no capitals here!
-                        console.log("Existing download link: " + downloadLink);
-                        fileService.emitFileUploaded(channelId, userId, isDM, existingFile.filename, downloadLink);
-                        resolve({ message: 'File already exists', downloadLink: downloadLink });
-                        fileExists = true;
-                        return;
-                    } else {
-
-                        var bytesUsed = numberFromPSQL(user.bytesUsed);
-                        var bytesAllowed = numberFromPSQL(user.bytesAllowed);
-                        const bytesAboutToBeUsed = bytesUsed + fileSize;
-
-                        console.log("FILE SIZE IS " + fileSize);
-                        console.log("BYTES USED IS " + bytesUsed);
-                        console.log("BYTES ALLOWED IS " + bytesAllowed);
-
-                        console.log("Bytes used plus file size: " + bytesAboutToBeUsed);
-                        if (bytesAboutToBeUsed >= bytesAllowed) {
-                            console.log("User exceeded storage limit");
-                            reject({ error: 'User has exceeded their storage limit', statusCode: 403 });
-                            return;
-                        }
-
-                        file.open = async function () {
-                            this._writeStream = new Transform({
-                                transform(chunk, encoding, callback) {
-                                    callback(null, chunk)
-                                }
-                            })
-
-                            this._writeStream.on('error', e => {
-                                form.emit('error', e)
-                            });
-
-                            // upload to S3
-                            new Upload({
-                                client: new S3Client({
-                                    credentials: {
-                                        accessKeyId,
-                                        secretAccessKey
-                                    },
-                                    region
-                                }),
-                                params: {
-                                    Bucket,
-                                    Key: `${guildId}/${userId}/${originalFilename}`,
-                                    Body: this._writeStream
-                                },
-                                tags: [], // optional tags
-                                queueSize: 4, // optional concurrency configuration
-                                partSize: 1024 * 1024 * 5, // optional size of each part, in bytes, at least 5MB
-                                leavePartsOnError: false, // optional manually handle dropped parts
-                            }).done()
-                                .then(data => {
-                                    form.emit('data', { name: "complete", value: data });
-
-                                    // add bytes to user
-                                    userModel.addBytes(userId, fileSize);
-
-                                    const expirationDate = user.isPremium ? null : new Date(Date.now() + 14 * 24 * 60 * 60 * 1000);
-
-                                    fileModel.insertFile(fileId, guildId, userId, originalFilename, fileHash, new Date(), expirationDate)
-                                        .then(() => {
-                                            const downloadLink = `${hostname}/download/${fileId}`;
-
-                                            fileService.emitFileUploaded(channelId, userId, isDM, file.originalFilename, downloadLink);
-                                            console.log(`Finished uploading to AWS: ${file.originalFilename} to guild ${guildId}, channel ${channelId}, user ${userId}`);
-
-                                            resolve({ downloadLink: downloadLink });
-                                        })
-                                        .catch(error => {
-                                            console.error('Error inserting file:', error);
-                                            reject(error);
-                                        });
-
-                                }).catch((err) => {
-                                    form.emit('error', err);
-                                })
-                        }
-
-                        file.end = function (cb) {
-                            this._writeStream.on('finish', () => {
-                                this.emit('end')
-                                cb()
-                            })
-                            this._writeStream.end()
-                        }
-
+            file.open = async function () {
+                this._writeStream = new Transform({
+                    transform(chunk, encoding, callback) {
+                        callback(null, chunk);
                     }
-                })
-                .catch(error => {
-                    console.error('Error getting file by hash:', error);
-                    reject(error);
-                    return;
                 });
 
+                this._writeStream.on('error', e => {
+                    console.error('Error: ', e);
+                    form.emit('error', e);
+                });
+
+                // upload to S3
+                new Upload({
+                    client: new S3Client({
+                        credentials: {
+                            accessKeyId,
+                            secretAccessKey
+                        },
+                        region
+                    }),
+                    params: {
+                        Bucket,
+                        Key: `${guildId}/${userId}/${originalFilename}`,
+                        Body: this._writeStream
+                    },
+                    tags: [], // optional tags
+                    queueSize: 4, // optional concurrency configuration
+                    partSize: 1024 * 1024 * 5, // optional size of each part, in bytes, at least 5MB
+                    leavePartsOnError: false, // optional manually handle dropped parts
+                }).done()
+                    .then(data => {
+                        form.emit('data', { name: "complete", value: data });
+
+                        // add bytes to user
+                        userModel.addBytes(userId, fileSize);
+
+                        const expirationDate = user.isPremium ? null : new Date(Date.now() + 14 * 24 * 60 * 60 * 1000);
+
+                        fileModel.insertFile(fileId, guildId, userId, originalFilename, fileHash, new Date(), expirationDate)
+                            .then(() => {
+                                const downloadLink = `${hostname}/download/${fileId}`;
+
+                                fileService.emitFileUploaded(channelId, userId, isDM, file.originalFilename, downloadLink);
+                                console.log(`Finished uploading to AWS: ${file.originalFilename} to guild ${guildId}, channel ${channelId}, user ${userId}`);
+
+                                resolve({ downloadLink: downloadLink });
+                            })
+                            .catch(error => {
+                                console.error('Error inserting file:', error);
+                                reject(error);
+                            });
+
+                    }).catch((err) => {
+                        form.emit('error', err);
+                    })
+            }
+
+            file.end = function (cb) {
+                this._writeStream.on('finish', () => {
+                    this.emit('end')
+                    cb()
+                })
+                this._writeStream.end()
+            }
         })
     })
 }
