9fdea81f78b0e53cf9f3eb903ed6fc520a3c1fcc
btarg <icrazyblaze1@gmail.com>
2024-05-31 17:40:02 +0100

    error handling

--- src/frontend/src/util.js
+++ src/frontend/src/util.js
@@ -2,14 +2,19 @@ export function formatMoney(number) {
     return number.toLocaleString('en-US', { style: 'currency', currency: 'USD' });
 }
 export function getJSONPayloadFromCookie(prefix) {
-    // signed json cookie
-    if (!prefix.endsWith("=s:j:")) {
-        prefix += "=s:j:"; 
-    }
+    try {
+        // signed json cookie
+        if (!prefix.endsWith("=s:j:")) {
+            prefix += "=s:j:"; 
+        }
 
-    const foundCookie = decodeURIComponent(document.cookie).split('; ').find(row => row.startsWith(prefix));
-    const decoded = decodeURIComponent(foundCookie).replace(prefix, '');
-    const payload = decoded.split('.')[0];
-    const json = JSON.parse(payload);
-    return json;
+        const foundCookie = decodeURIComponent(document.cookie).split('; ').find(row => row.startsWith(prefix));
+        const decoded = decodeURIComponent(foundCookie).replace(prefix, '');
+        const payload = decoded.split('.')[0];
+        const json = JSON.parse(payload);
+        return json;
+    } catch (error) {
+        console.error('Error parsing JSON payload from cookie:', error);
+        return null;
+    }
 }
