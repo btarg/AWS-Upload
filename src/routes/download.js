df2f2ad46c31ea055d2eb8f711041878fde5185d
btarg <icrazyblaze1@gmail.com>
2024-06-02 00:32:33 +0100

    cleanup

--- src/routes/download.js
+++ src/routes/download.js
@@ -7,8 +7,6 @@ dotenv.config();
 import { GetObjectCommand } from "@aws-sdk/client-s3";
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 import rateLimit from "express-rate-limit";
-
-import { getFileById } from '../services/fileService.js';
 import { getFriendlyFileType, getThumbnailUrl } from '../utils/fileutil.js';
 import { b2Client, Bucket } from '../config/backblaze.js';
 
